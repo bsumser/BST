6fce6d8ed8b64cdc635bfde53b05fbcaaa9e7afe
bsumserLaptop <bsumser@uoregon.edu>
2019-02-07 14:09:38 -0800

    added skeleton code and finished file read to check for command

---
+++ BST.py
@@ -0,0 +1,65 @@
+from Node import Node
+
+class BST(object):
+    def __init__(self):
+        self.__root = None
+
+    def getRoot(self):
+        # Private Method, can only be used inside of BST.
+        return self.__root
+
+    def __findNode(self, data):
+        # Private Method, can only be used inside of BST.
+        # Search tree for a node whose data field is equal to data.
+        # Return the Node object
+        pass
+
+    def contains(self, data):
+        # return True of node containing data is present in the tree.
+        # otherwise, return False.
+        pass
+
+    def insert(self, data):
+        # Find the right spot in the tree for the new node
+        # Make sure to check if anything is in the tree
+        # Hint: if a node n is null, calling n.getData() will cause an error
+        pass
+    
+    def delete(self, data):
+        # Find the node to delete.
+        # If the value specified by delete does not exist in the tree, then don't change the tree.
+        # If you find the node and ...
+        #  a) The node has no children, just set it's parent's pointer to Null.
+        #  b) The node has one child, make the nodes parent point to its child.
+        #  c) The node has two children, replace it with its successor, and remove 
+        #       successor from its previous location.
+        # Recall: The successor of a node is the left-most node in the node's right subtree.
+        # Hint: you may want to write a new method, findSuccessor() 
+	#to find the successor when there are two children
+
+        return None
+
+    def __findSuccessor(self, aNode):
+        pass
+
+    def traverse(self, order, top):
+        # traverse the tree by printing out the node data for all node in a specified order
+
+        if top is not None:
+            if order == "preorder":
+                # your code here, remove pass
+                pass
+                
+            
+            elif order == "inorder":
+                # your code here, remove pass
+                pass
+
+
+            elif order == "postorder":
+                # your code here, remove pass
+                pass
+            
+            else:
+                print("Error, order {} undefined".format(order))
+
---
+++ lab2.py
@@ -0,0 +1,30 @@
+#Brett Sumser
+
+from sys import argv
+from BST import BST
+
+def main(argv):
+    # Loop over input file (filename passed via argv).
+    input_file = argv[1]
+    with open(input_file, 'r') as file_ob:
+    # Split each line into a task and number (if one exists) 
+    #hint: use the split method for strings 
+    # Perform the function corresponding to the specified task
+    # i.e., insert, delete, inorder, preorder, postorder
+    # Close the file when you're done.
+        for line in file_ob:
+            if ' ' in line:
+                command, num = line.split()
+                print command
+                print num
+            else:
+                command = line
+                print command
+            #print(line.split()) 
+            if 'str' in line:
+                break
+    pass
+
+if __name__ == "__main__":
+    main(argv)
+
